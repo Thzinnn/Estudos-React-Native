07c330b767d6a020769b30e72d70a12a1141b2e3
Thiago <thiagoil2006@gmail.com>
2024-02-08 17:50:29 -0300

    aula 08/02/2024

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+console.log("test")
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Deus é bom e o Diabo não presta</Text>
+      <Text style={styles.boasvindas}>Deus é bom e o Diabo não presta</Text>
+      <Text style={styles.outrotexto}>O Povo não quer picanha, o povo quer chapaaar</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +20,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  boasvindas: {
+    color:"#568975",
+    backgroundColor:"DDDDDD",
+    fontSize: 30
+  },
+
+  outrotexto: {
+    fontStyle: "italic",
+    fontSize: 25,
+    fontWeight: "700",
+    color: "GGGGGG",
+
+  }
 });
+
