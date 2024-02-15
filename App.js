e8c91901c750ce3a5706937d0cb66d4dd9eca00f
Thiago <thiagoil2006@gmail.com>
2024-02-15 17:37:48 -0300

    views e images - aula 15/02/24

--- App.js
+++ App.js
@@ -1,13 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 console.log("test")
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.boasvindas}>Deus é bom e o Diabo não presta</Text>
-      <Text style={styles.outrotexto}>O Povo não quer picanha, o povo quer chapaaar</Text>
+      <View style={styles.header}>
+        <View style={styles.flex_header}>
+          <Image
+            style={styles.imagem}
+            source={{
+              uri: 'https://www.mjornal.com.br/wp-content/uploads/2023/09/gatinho-678x381.jpg',
+            }}
+          />
+          <Text style={styles.texto_header}>Gatinho</Text>
+        </View>
+      </View>
+      <View style={styles.main}>
+        <Text style={styles.outrotexto}>Essa é a história de um anão que dishavava, fumou da erva fina e bateu uma onda forte, ele foi pro paraiso e na fachada tava escito oq          BATALHAAA DA NORTEE</Text>
+      </View>
+      <View style={styles.footer}>
+        <Text style={styles.textos}>copyright@2024</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -21,18 +36,66 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 
+  header: {
+    flex: 1.7,
+    width: '100%',
+    backgroundColor: '#07436e',
+    justifyContent: 'center',
+  },
+
+  main: {
+    flex: 9,
+    width: '100%',
+    justifyContent: 'center',
+  },
+
+  footer: {
+    flex: 1,
+    width: '100%',
+    backgroundColor: '#07436e',
+    justifyContent: 'flex-end',
+  },
+
   boasvindas: {
-    color:"#568975",
-    backgroundColor:"DDDDDD",
+    color: "#568975",
+    backgroundColor: "DDDDDD",
     fontSize: 30
   },
 
   outrotexto: {
     fontStyle: "italic",
-    fontSize: 25,
-    fontWeight: "700",
-    color: "GGGGGG",
+    fontSize: 20,
+    textAlign: 'center',
+  },
 
+  textos: {
+    fontStyle: 'italic',
+    color: '#fff',
+    fontSize: 10,
+    textAlign: 'left',
+    marginLeft: 10,
+    marginBottom: 5,
+  },
+
+  texto_header: {
+    fontStyle: 'italic',
+    color: '#fff',
+    fontSize: 20,
+    textAlign: 'left',
+    textAlignVertical: 'center'
+  },
+
+  imagem: {
+    width: 50,
+    height: 50,
+    marginHorizontal: 10,
+    borderRadius: 50,
+  },
+
+  flex_header: {
+    display: 'flex',
+    flexDirection: 'row',
+    marginTop: 40,
   }
 });
 
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Estudos React native
+
+# Comandos Uteis
+
+## Criar um novo projeto React Native com Expo
+
+```
+npx create-expo-app --template
+```
+
+### Testar no celular o Tunnel
+
+1) Baixar o App Expo Go
+
+https://play.google.com/store/apps/details?id=host.exp.exponent&hl=pt_BR&gl=US
